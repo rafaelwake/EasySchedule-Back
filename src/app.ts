c1185582025e8601b7b1e53f15dee531c858ff25
Rafael Bernardes <60991402+rafaelwake@users.noreply.github.com>
2023-04-27 23:52:29 -0300

    start test using postman

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("testing");
+import express from "express";
+const app = express();
+
+// ENV Variables
+import _default from "../config/default";
+
+// Routes
+import router from "./router";
+
+const port = _default.port;
+
+app.use("/api/", router);
+
+app.listen(port, async () => {
+  console.log(`App rodando na porta: ${port}`);
+});
